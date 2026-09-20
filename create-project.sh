85a1873567b146ae083f7a16dcecd9e591e36447
dream0x <parasol.sut.crt@gmail.com>
2026-09-20 16:24:13 +0900

    misc: 初期化設定追加

--- create-project.sh
+++ create-project.sh
@@ -2,19 +2,28 @@
 
 set -euo pipefail
 
-## create project
+## front
+
+docker compose exec front bash -c '
+set -euo pipefail
+
+echo "Creating project"
 npx create-expo-app -e with-router .
 
-## install dependencies
-npx expo install @biomejs/biome axios zustand \
+echo "Installing dependencies"
+npx expo install @biomejs/biome \
+  axios zustand \
   tamagui @tamagui/config react-native-svg @tamagui/lucide-icons-2 react-native-reanimated @tamagui/animations-reanimated \
   @react-native-async-storage/async-storage
 
-## build structure
-cp -r template/* .
+echo "Initializing project"
+npx @biomejs/biome init
 
-## start server
+echo "Starting server"
 npx expo start --lan -p 3000
 
-## stop server
-docker exec front pkill -f "expo start --lan -p 3000"
+# stop server
+# docker exec front pkill -f "expo start --lan -p 3000"
+'
+
+## back
